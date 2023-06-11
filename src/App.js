fa5e9eec3f508730c2225cb2e8b4cd33dc371828
hetpatel4381 <hetpatel2808hp@gmail.com>
2023-06-12 01:40:25 +0530

    completed the MovieLand

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ const API_URL = "https://www.omdbapi.com?apikey=91781fae";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
@@ -27,10 +28,14 @@ function App() {
       <div className="search">
         <input
           placeholder="Search for Movies"
-          value="Superman"
-          onChange={() => {}}
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
-        <img src={SearchIcon} alt="search" onClick={() => {}} />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
       </div>
 
       {movies?.length > 0 ? (
